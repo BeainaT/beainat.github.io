75206b35c2c48afff2d13e3d24755cdc8d200a67
BeainaT <tania.df@hotmail.it>
2022-04-28 19:06:40 +0200

    fixed variable in if

--- js/main.js
+++ js/main.js
@@ -4,17 +4,15 @@ const distanceTravel = Number(prompt("Quanti km devi percorrere?"));
 const userAge = Number(prompt("Quanti anni hai?"));
 //set ticket price by distance
 const sum = (0.21 * distanceTravel);
-//then consider price by 20% discount if user age < 18, 40% discount if user age >= 65
-let amount
-
+//then consider price by 20% discount if user age < 18, 40% discount if user age >= 65 and print on document
+let amount;
 if (userAge < 18) {
-    let amount = sum - (sum * 0.2);
+    amount = sum - (sum * 0.2);
     document.getElementById("price").innerHTML = `${amount.toFixed(2)} €`;
 } else if (userAge >= 65) {
-    let amount = sum - (sum * 0.4);
+    amount = sum - (sum * 0.4);
     document.getElementById("price").innerHTML = `${amount.toFixed(2)} €`;
 } else {
     document.getElementById("price").innerHTML = `${sum.toFixed(2)} €`;
 }
-
 document.getElementById("user").innerHTML = `${"questo è il prezzo del tuo biglietto:"}`;
